a3a70edbe0fd58ffae5bbaa3dd6e99e9d72b1af7
2theMoon <dlaguddh1@gmail.com>
2022-01-21 17:02:24 +0900

    퀴즈 라우팅 숙제 완료

--- template/src/Components/Score.js
+++ template/src/Components/Score.js
@@ -4,27 +4,46 @@ function Score() {
     const location = useLocation();
     const quizList = location.state.quizList;
     const answer = location.state.answer;
-    console.log(quizList);
+
+    let resultArr = [];
+    for (let i = 0; i < answer.length; i++) {
+        if (quizList[i].answer === answer[i]) {
+            let result = true;
+            resultArr.push(result);
+        } else {
+            let result = false;
+            resultArr.push(result);
+        }
+        console.log(resultArr);
+    }
 
     return (
         <div>
             <h1> 결과 점수 </h1>
             <ul>
                 {quizList.map((list, i) => {
-                    const result = list.answer === true ? "O" : "X"
+                    const result = list.answer === true ? "O" : "X";
                     return (
                         <>
-                            <li>{result}</li>
+                            <div>{i + 1}번 문제 정답 : {result}</div>
                         </>
                     );
                 })}
                 {answer.map((list, i) => {
-                    const result = list === true ? "O" : "X"
+                    const result = list === true ? "O" : "X";
                     return (
                         <>
-                            <li>{i + 1}번 문제 정답 : {result}</li>
+                            <div>{i + 1}번 고른 답 : {result}</div>
                         </>
                     )
+                })}
+                {resultArr.map((list, i) => {
+                    const result = list === true ? "정답!" : "오답ㅠ";
+                    return (
+                        <>
+                            <div>{i + 1}번 {result}</div>
+                        </>
+                    );
                 })}
             </ul>
         </div>
