0829da0d00d5e7e94889e78c103d87f0b4624c5f
2theMoon <dlaguddh1@gmail.com>
2022-01-21 12:53:01 +0900

    배열 데이터 뽑기

--- template/src/Components/Quiz.js
+++ template/src/Components/Quiz.js
@@ -1,22 +1,37 @@
-import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
+import { Link, Route, useHistory } from "react-router-dom";
 
 const quiz_list = [
-    { question: '세균이나 바이러스는 일반적으로 열에 강하다', answer: false },
-    { question: '약은 따뜻한 물과 함께 복용해야 효과적이다', answer: true },
-    { question: '코로나바이러스는 공기만으로 전염된다', answer: false },
-    { question: '사람의 5가지 (시각, 후각, 미각, 청각, 촉각) 충에서 가장 먼저 나빠지는 감각기관은 시각이다', answer: true }
+    { id: 1, question: '세균이나 바이러스는 일반적으로 열에 강하다', answer: false },
+    { id: 2, question: '약은 따뜻한 물과 함께 복용해야 효과적이다', answer: true },
+    { id: 3, question: '코로나바이러스는 공기만으로 전염된다', answer: false },
+    { id: 4, question: '사람의 5가지 (시각, 후각, 미각, 청각, 촉각) 충에서 가장 먼저 나빠지는 감각기관은 시각이다', answer: true }
 ];
+let answerArr = [];
 
 function Quiz() {
+    const [currentNo, setCurrentNo] = useState(0);
     const history = useHistory();
     const setAnswer = (user) => {
-        console.log(user);
+        answerArr.push(user);
+
+        if (currentNo === quiz_list.length - 1) {
+            history.push({
+                pathname: "/score",
+                state: { answer: answerArr, quizList: quiz_list },
+            });
+        } else {
+            cureent();
+        }
+    }
+    const cureent = () => {
+        setCurrentNo((prev) => prev + 1);
     }
 
     return (
         <div>
-            <h3>1번 문제</h3>
-            <p>{quiz_list[0].question}</p>
+            <h3>{`${quiz_list[currentNo].id} 번 문제`}</h3>
+            <p>{quiz_list[currentNo].question}</p>
             <button onClick={() => setAnswer(true)}>O</button>
             <button onClick={() => setAnswer(false)}>X</button>
         </div>
--- template/src/Components/Score.js
+++ template/src/Components/Score.js
@@ -1,10 +1,32 @@
+import { useLocation, useParams } from "react-router-dom";
 
 function Score() {
+    const location = useLocation();
+    const quizList = location.state.quizList;
+    const answer = location.state.answer;
+    console.log(quizList);
 
     return (
         <div>
-            <h1> 점수 </h1>
-            <p>점수 발표</p>
+            <h1> 결과 점수 </h1>
+            <ul>
+                {quizList.map((list, i) => {
+                    const result = list.answer === true ? "O" : "X"
+                    return (
+                        <>
+                            <li>{result}</li>
+                        </>
+                    );
+                })}
+                {answer.map((list, i) => {
+                    const result = list === true ? "O" : "X"
+                    return (
+                        <>
+                            <li>{i + 1}번 문제 정답 : {result}</li>
+                        </>
+                    )
+                })}
+            </ul>
         </div>
     );
 }
