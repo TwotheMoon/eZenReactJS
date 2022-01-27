9c49a547415dcaa1269c610c0b52430079e70789
2theMoon <dlaguddh1@gmail.com>
2022-01-27 20:34:56 +0900

    기존 js 에서 fetch, async, await

---
+++ Day11/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import ReducerUseContext from './ReducerUseContext';
+// import UseContextHook from './UseContextHook';
+// import Props from './Props';
+// import UseReducerHook from './UseReducerHook';
+// import UseStateHook from './UseStateHook';
+
+function App() {
+
+  return (
+    <div className='app'>
+      {/* <UseStateHook /> */}
+      {/* <UseReducerHook /> */}
+      {/* <Props /> */}
+      {/* <UseContextHook /> */}
+      <ReducerUseContext />
+    </div>
+  );
+}
+export default App;
--- template/src/App.js
+++ template/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import ReducerUseContext from './ReducerUseContext';
-// import UseContextHook from './UseContextHook';
-// import Props from './Props';
-// import UseReducerHook from './UseReducerHook';
-// import UseStateHook from './UseStateHook';
 
 function App() {
 
   return (
     <div className='app'>
-      {/* <UseStateHook /> */}
-      {/* <UseReducerHook /> */}
-      {/* <Props /> */}
-      {/* <UseContextHook /> */}
-      <ReducerUseContext />
+
     </div>
   );
 }
