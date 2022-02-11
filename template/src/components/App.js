eb670a9bd82692b9d80114d1c6402f3efb379853
2theMoon <dlaguddh1@gmail.com>
2022-02-11 21:57:37 +0900

    최신순 베스트순 정렬

--- template/src/components/App.js
+++ template/src/components/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import items from '../db/mock.json';
 import ReviewList from './ReviewList';
+import { useState } from "react";
 
 function App() {
-  const sortedItems = items.sort((a, b) => b.rating - a.rating);
+  const [order, setOrder] = useState();
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  const onRecentClick = () => {
+    setOrder("createdAt");
+  }
+  const onBestClick = () => {
+    setOrder("rating");
+  }
   return (
     <div>
+      <button onClick={onRecentClick}>최신순</button>
+      <button onClick={onBestClick}>베스트순</button>
       <ReviewList items={sortedItems} />
     </div>
   );
