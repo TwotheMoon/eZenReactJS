59bba277aecf830a1c178e17ac3d11a36ad6f523
2theMoon <dlaguddh1@gmail.com>
2022-02-23 17:15:35 +0900

    -input value는 직접 변경X 부모에서 props 형태로 전달 받아 사용해야함 - 이미지 인풋 컴포넌트 생성

--- template/src/components/ReviewForm.js
+++ template/src/components/ReviewForm.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import FileInput from "./FileInput";
 import "./reviewForm.css";
 
 function ReviewForm() {
     const [values, setValues] = useState({
         title: '',
         rating: 0,
-        content: ''
+        content: '',
+        imgFile: null
     });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target; // 태그 name, value값을 가져와서
-        setValues((prevValues) => ({      // 미리 만들어둔 객체안에
-            ...prevValues, [name]: value  // key, value = 값으로 넣기
+    const handleChange = (name, value) => {
+        setValues((values) => ({      // 미리 만들어둔 객체안에
+            ...values, [name]: value  // key, value = 값으로 넣기
         }));
+    }
+    const handleInputChange = (e) => {
+        const { name, value } = e.target; // 태그 name, value값을 가져와서
+        handleChange(name, value);
     }
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,15 +25,18 @@ function ReviewForm() {
         setValues({
             title: '',
             rating: 0,
-            content: ''
+            content: '',
+            imgFile: ''
         });
     }
 
+
     return (
         <form className="reviewForm" onSubmit={handleSubmit}>
-            <input name="title" placeholder="title" value={values.title} onChange={handleChange} />
-            <input name="rating" type="number" value={values.rating} onChange={handleChange} />
-            <textarea name="content" value={values.content} onChange={handleChange} />
+            <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
+            <input name="title" placeholder="title" value={values.title} onChange={handleInputChange} />
+            <input name="rating" type="number" value={values.rating} onChange={handleInputChange} />
+            <textarea name="content" value={values.content} onChange={handleInputChange} />
             <button type="submit">확인</button>
         </form>
     );
