13b0881cb6195edc881d668dcaa33a3cf7add05d
2theMoon <dlaguddh1@gmail.com>
2022-02-24 20:04:49 +0900

    Create FileInput.js

---
+++ template/src/components/FileInput.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function FileInput({ name, value, onChange }) {
+    const handleChange = (e) => {
+        const nextValue = e.target.files[0];
+        onChange(name, nextValue);
+        console.log(value);
+    }
+
+    return (
+        <input type="file" onChange={handleChange} />
+    );
+}
+export default FileInput;
